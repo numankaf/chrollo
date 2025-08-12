e6e887c746a19121400666a563e40a35a66b1fbb
Numan Kafadar <numankafadar97@gmail.com>
2025-08-12 18:29:15 +0300

    initial: theme variables added

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,19 @@
-import { Client } from '@stomp/stompjs';
-import { useEffect, useRef } from 'react';
-import SockJS from 'sockjs-client';
+import { use } from 'react';
+import './App.css';
+import { ThemeProviderContext } from './context/ThemeProviderContext';
 
 function App() {
-  const stompClientRef = useRef<Client | null>(null);
+  const { theme, setTheme } = use(ThemeProviderContext);
 
-  useEffect(() => {
-    const stompClient = new Client({
-      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
-      reconnectDelay: 5000,
-      debug: (msg) => console.log(msg),
-    });
-
-    stompClient.onConnect = (frame) => {
-      console.log('✅ Connected:', frame);
-
-      stompClient.subscribe('/topic/scope-bsi-command', (message) => {
-        console.log('📩 Received:', message.body);
-      });
-      stompClient.subscribe('/topic/scope-bsi-event', (message) => {
-        console.log('📩 Received:', message.body);
-      });
-    };
-
-    stompClient.onStompError = (frame) => {
-      console.error('❌ Broker reported error:', frame.headers['message']);
-      console.error('❌ Additional details:', frame.body);
-    };
-
-    stompClient.onWebSocketClose = (event) => {
-      console.log('🔌 WebSocket closed', event);
-    };
-
-    stompClient.activate();
-    stompClientRef.current = stompClient;
-
-    return () => {
-      stompClient.deactivate();
-      console.log('🔌 STOMP client deactivated');
-    };
-  }, []);
-
-  const sendMessage = (message: string) => {
-    if (stompClientRef.current && stompClientRef.current.connected) {
-      stompClientRef.current.publish({
-        destination: '/app/topic/scope-bsi-command/545445', // adjust to your server endpoint
-        body: message,
-        // headers: { /* any headers if needed */ },
-      });
-      console.log('Message sent:', message);
-    } else {
-      console.error('WebSocket not connected');
-    }
-  };
   return (
-    <div>
-      <button className="bg-red-500 p-3 rounded-md cursor-pointer" onClick={() => sendMessage('Some Bsi Message')}>
-        Send Bsi Message
+    <div className="bg-background">
+      <button
+        className=" p-3 rounded-md cursor-pointer bg-primary"
+        onClick={() => (theme === 'dark' ? setTheme('light') : setTheme('dark'))}
+      >
+        Change Theme
       </button>
+      <div>Subscriptions</div>
     </div>
   );
 }
