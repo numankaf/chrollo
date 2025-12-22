b3b1246d064e2d55295da95ed812ce9508da153b
Numan Kafadar <numankafadar97@gmail.com>
2025-12-22 21:51:47 +0300

    chore: Update vite config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,58 +2,60 @@ import path, { resolve } from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import ReactCompiler from 'babel-plugin-react-compiler';
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import { defineConfig, externalizeDepsPlugin, loadEnv } from 'electron-vite';
 import svgr from 'vite-plugin-svgr';
 
-import packageJson from './package.json';
+const DEFAULT_PORT = 3000;
 
-const DEFAULT_PORT = '3000';
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()],
-    resolve: {
-      alias: {
-        '@/types': resolve('src/types'),
-        '@/main': resolve('src/main'),
-      },
-    },
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()],
-    resolve: {
-      alias: {
-        '@/types': resolve('src/types'),
-      },
-    },
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@/resources': path.resolve(__dirname, 'resources'),
-        '@/types': path.resolve(__dirname, 'src/types'),
-        '@': path.resolve(__dirname, 'src/renderer/src'),
-      },
-    },
-    server: {
-      host: true,
-      port: parseInt(process.env.VITE_PORT ?? DEFAULT_PORT),
-      watch: {
-        usePolling: true,
-      },
-    },
-    define: {
-      global: 'globalThis',
-      APP_VERSION: JSON.stringify(packageJson.version),
-    },
-    plugins: [
-      react({
-        babel: {
-          plugins: [ReactCompiler],
+  return {
+    main: {
+      plugins: [externalizeDepsPlugin()],
+      resolve: {
+        alias: {
+          '@/types': resolve('src/types'),
+          '@/main': resolve('src/main'),
         },
-      }),
-      tailwindcss(),
-      svgr(),
-    ],
-  },
+      },
+    },
+    preload: {
+      plugins: [externalizeDepsPlugin()],
+      resolve: {
+        alias: {
+          '@/types': resolve('src/types'),
+        },
+      },
+    },
+    renderer: {
+      resolve: {
+        alias: {
+          '@/resources': path.resolve(__dirname, 'resources'),
+          '@/types': path.resolve(__dirname, 'src/types'),
+          '@': path.resolve(__dirname, 'src/renderer/src'),
+        },
+      },
+      server: {
+        host: true,
+        port: Number(env.VITE_PORT ?? DEFAULT_PORT),
+        watch: {
+          usePolling: true,
+        },
+      },
+      define: {
+        global: 'globalThis',
+        APP_VERSION: JSON.stringify(env.VITE_APP_VERSION) ?? 'dev',
+      },
+      plugins: [
+        react({
+          babel: {
+            plugins: [ReactCompiler],
+          },
+        }),
+        tailwindcss(),
+        svgr(),
+      ],
+    },
+  };
 });
