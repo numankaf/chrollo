4d3e0a454439d8274db688903dae282cdd233505
Numan Kafadar <numankafadar97@gmail.com>
2025-12-23 22:28:55 +0300

    fix(editor): Fix code editor body type reactivity issue

--- src/renderer/src/utils/socket-message-util.ts
+++ src/renderer/src/utils/socket-message-util.ts
@@ -1,17 +1,13 @@
 import { REQUEST_BODY_TYPE, type RequestBodyType } from '@/types/collection';
 
 function getMessageContentType(headers?: Record<string, string>): RequestBodyType {
-  console.log(headers);
   if (!headers) return REQUEST_BODY_TYPE.TEXT;
 
   const contentType = headers['content-type'] ?? headers['Content-Type'] ?? headers['CONTENT-TYPE'];
-  console.log(contentType);
 
   if (!contentType) return REQUEST_BODY_TYPE.TEXT;
-  console.log(contentType.includes('application/json') || contentType.includes('+json'));
 
   if (contentType.includes('application/json') || contentType.includes('+json')) {
-    console.log('then you should return kson');
     return REQUEST_BODY_TYPE.JSON;
   }
 
